64a03b089067e169062dfe6560ef2da179cb129e
fraclysis <fraclysis@gmail.com>
2025-11-23 22:17:11 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,14 @@ use std::{
     time::Duration,
 };
 
+#[cfg(target_os = "windows")]
+use std::os::windows::process::CommandExt;
+
+#[cfg(all(not(target_os = "hermit"), any(unix)))]
+use nix::sys::signal::{SigHandler, Signal, signal};
+#[cfg(all(not(target_os = "hermit"), any(unix)))]
+use std::os::unix::process::CommandExt;
+
 use clap::Parser;
 use walkdir::WalkDir;
 
@@ -19,6 +27,13 @@ struct State {
     jobs: VecDeque<(PathBuf, PathBuf)>,
     cancel: bool,
     done: bool,
+}
+
+#[derive(Clone, Copy)]
+enum ArgMode {
+    Image,
+    Video,
+    Custom,
 }
 
 fn main() {
@@ -30,7 +45,18 @@ fn main() {
             .get(),
     );
 
-    let exts = ["png", "jpg", "jpeg", "bmp", "tiff", "gif"];
+    let img_exts = ["png", "jpg", "jpeg", "bmp", "tiff", "gif"];
+
+    let vid_exts = [
+        "mp4", "mkv", "mov", "avi", "webm", "flv", "wmv", "m4v", "mpg", "mpeg", "3gp", "ts",
+        "m2ts", "mts", "ogv", "f4v", "vob", "asf", "rm", "rmvb",
+    ];
+
+    let exts = if cli.video {
+        vid_exts.as_slice()
+    } else {
+        img_exts.as_slice()
+    };
 
     let mut counter = 0;
     let mut output = PathBuf::from(cli.log);
@@ -59,6 +85,7 @@ fn main() {
         let pair = pair.clone();
 
         ctrlc::set_handler(move || {
+            eprintln!("CONTROL C");
             let (mutex, cond, _) = &*(pair);
             mutex.lock().unwrap().cancel = true;
             cond.notify_all();
@@ -68,9 +95,20 @@ fn main() {
 
     let mut threads = Vec::with_capacity(core);
 
+    let arg_mode = if cli.args.is_some() {
+        ArgMode::Custom
+    } else if cli.video {
+        ArgMode::Video
+    } else {
+        ArgMode::Image
+    };
+
+    let use_extension = if cli.video { "webm" } else { "webp" };
+
     for _ in 0..core {
         let pair = pair.clone();
         let dry = cli.dry;
+        let video = cli.video;
 
         let thread = std::thread::spawn(move || {
             loop {
@@ -98,28 +136,79 @@ fn main() {
                 }
 
                 if let Some(work) = work {
+                    let arg_image = [
+                        OsStr::new("-hide_banner"),
+                        OsStr::new("-n"),
+                        OsStr::new("-i"),
+                        work.0.as_os_str(),
+                        OsStr::new("-vcodec"),
+                        OsStr::new("libwebp"),
+                        OsStr::new("-qscale"),
+                        OsStr::new("80"),
+                        work.1.as_os_str(),
+                    ];
+
+                    let arg_video = [
+                        OsStr::new("-hide_banner"),
+                        OsStr::new("-n"),
+                        OsStr::new("-i"),
+                        work.0.as_os_str(),
+                        OsStr::new("-c:v"),
+                        OsStr::new("libvpx-vp9"),
+                        OsStr::new("-crf"),
+                        OsStr::new("30"),
+                        OsStr::new("-b:v"),
+                        OsStr::new("0"),
+                        OsStr::new("-b:a"),
+                        OsStr::new("128k"),
+                        OsStr::new("-c:a"),
+                        OsStr::new("libopus"),
+                        OsStr::new("-row-mt"),
+                        OsStr::new("1"),
+                        work.1.as_os_str(),
+                    ];
+
+                    let args = match arg_mode {
+                        ArgMode::Image => arg_image.as_slice(),
+                        ArgMode::Video => arg_video.as_slice(),
+                        ArgMode::Custom => {
+                            todo!()
+                        }
+                    };
+
                     if dry != 0 {
-                        println!(
-                            "ffmpeg -i {:?} -vcodec libwebp -qscale 80 {:?}",
-                            work.0, work.1
-                        );
+                        print!("ffmpeg");
+                        for arg in args {
+                            print!("{:?}", arg);
+                        }
+                        print!("\n");
 
                         sleep(Duration::from_millis(dry as _));
                     } else {
-                        let status = Command::new("ffmpeg")
-                            .args([
-                                OsStr::new("-hide_banner"),
-                                OsStr::new("-n"),
-                                OsStr::new("-i"),
-                                work.0.as_os_str(),
-                                OsStr::new("-vcodec"),
-                                OsStr::new("libwebp"),
-                                OsStr::new("-qscale"),
-                                OsStr::new("80"),
-                                work.1.as_os_str(),
-                            ])
-                            .output()
-                            .unwrap();
+                        let mut command = Command::new("ffmpeg");
+                        command.args(args);
+
+                        #[cfg(target_os = "windows")]
+                        command.creation_flags(0x00000200);
+
+                        #[cfg(all(not(target_os = "hermit"), any(unix)))]
+                        command.pre_exec(|| {
+                            // Ignore SIGINT in the child
+                            unsafe {
+                                signal(Signal::SIGINT, SigHandler::SigIgn).unwrap();
+                            }
+                            Ok(())
+                        });
+
+                        if video {
+                            println!("Start {:?} {:?}", work.0, work.1);
+                        }
+
+                        let status = if video {
+                            command.spawn().unwrap().wait_with_output().unwrap()
+                        } else {
+                            command.output().unwrap()
+                        };
 
                         {
                             let mut file = pair.2.lock().unwrap();
@@ -128,14 +217,19 @@ fn main() {
                         }
 
                         if status.status.success() {
+                            if video {
+                                print!("End: ")
+                            }
+
                             println!("{:?} {:?}", work.0, work.1);
                             std::fs::remove_file(work.0).unwrap();
                         } else {
-                            eprintln!(
-                                "Failed: ffmpeg -i {:?} -vcodec libwebp -qscale 80 {:?}",
-                                work.0, work.1
-                            );
+                            eprintln!("Failed: {:?} {:?}", work.0, work.1);
                             std::io::stderr().write_all(&status.stderr).unwrap();
+
+                            if video {
+                                std::fs::remove_file(work.1).unwrap();
+                            }
                         }
                     }
                 }
@@ -166,12 +260,13 @@ fn main() {
             let (mutex, cond, _) = &*(pair);
 
             let mut counter = 0;
-            let mut output = path.with_extension("webp");
+            let mut output = path.with_extension(use_extension);
 
             if output.exists() || outputs.contains(&output) {
                 loop {
                     let file_name = output.file_stem().unwrap_or_default().to_string_lossy();
-                    let new_output = output.with_file_name(format!("{file_name}_{counter}.webp"));
+                    let new_output =
+                        output.with_file_name(format!("{file_name}_{counter}.{use_extension}"));
 
                     counter += 1;
 
@@ -224,4 +319,9 @@ struct Cli {
 
     #[arg(default_value_t = String::from("ffpack.txt"), short = 'l')]
     log: String,
+
+    #[arg(default_value_t = false, short = 'v')]
+    video: bool,
+
+    args: Option<String>,
 }
